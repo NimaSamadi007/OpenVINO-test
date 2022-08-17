f27bf8a6685b02ac92c69aa72993b81e2af08261
Nima Samadi <nbr.smd@gmail.com>
2022-08-17 16:23:41 +0430

    Dynamic shape input handled

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,11 @@ int main(){
     ov::Core core;
     ov::CompiledModel compiled_model = core.compile_model("/home/nima/Documents/w/retina/openvino-test/b12_fp32/b12.xml", "AUTO");
     ov::InferRequest infer_request = compiled_model.create_infer_request();
-    float memory_ptr[1*3*200*300];
+    float* memory_ptr = new float[1*3*640*480];
     auto input_port = compiled_model.input();
-    ov::Tensor input_tensor(input_port.get_element_type(),
-                            input_port.get_shape(),
-                            memory_ptr);
+    ov::Tensor input_tensor = ov::Tensor(input_port.get_element_type(), {1, 3, 640, 480}, memory_ptr);
     infer_request.set_input_tensor(input_tensor);
     infer_request.start_async();
     infer_request.wait();
+    delete[] memory_ptr;
 }
