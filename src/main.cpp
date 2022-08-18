5dd0b2d7a44c6c41de05bd5813c6d190ab2b307e
Nima Samadi <nbr.smd@gmail.com>
2022-08-18 10:34:11 +0430

    FP16 model added and tested - inference time tested

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
+#include <chrono>
 #include <openvino/openvino.hpp>
 
 int main(){
     ov::Core core;
-    ov::CompiledModel compiled_model = core.compile_model("/home/nima/Documents/w/retina/openvino-test/b12_fp32/b12.xml", "AUTO");
+    ov::CompiledModel compiled_model = core.compile_model("/home/nima/Documents/w/retina/openvino-test/b12_fp16/b12.xml", "AUTO");
     ov::InferRequest infer_request = compiled_model.create_infer_request();
     float* memory_ptr = new float[1*3*640*480];
     auto input_port = compiled_model.input();
     auto output_port = compiled_model.output();
     ov::Tensor input_tensor = ov::Tensor(input_port.get_element_type(), {1, 3, 640, 480}, memory_ptr);
     infer_request.set_input_tensor(input_tensor);
+
+    auto start = std::chrono::high_resolution_clock::now();
     infer_request.start_async();
     infer_request.wait();
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+    printf("%f\n", elapsed.count() / (1000000000.0));
+
     auto output = infer_request.get_tensor(output_port.get_any_name());
     float* output_buffer = output.data<float>();
     ov::Shape shape = output.get_shape();
